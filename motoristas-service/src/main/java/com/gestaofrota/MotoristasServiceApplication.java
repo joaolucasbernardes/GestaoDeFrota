e4912cf83830e9b6db5585520fb591290a08c2ad
Joao Lucas <joaolucasbernardes@gmail.com>
2025-07-17 11:50:59 -0300

    build: configura a estrutura inicial do projeto multi-módulo

---
+++ motoristas-service/src/main/java/com/gestaofrota/MotoristasServiceApplication.java
@@ -0,0 +1,11 @@
+package com.gestaofrota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoristasServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MotoristasServiceApplication.class, args);
+    }
+}
---
+++ veiculos-service/src/main/java/com/gestaofrota/VeiculosServiceApplication.java
@@ -0,0 +1,11 @@
+package com.gestaofrota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeiculosServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VeiculosServiceApplication.class, args);
+    }
+}
